ef876de0504d455f5cd1940ab237687e9d0507a6
amarrerod <alemarrerodiaz@gmail.com>
2023-06-07 11:09:58 +0100

    ✨ Introduces MaP heuristic

--- src/heuristic.rs
+++ src/heuristic.rs
@@ -1,5 +1,6 @@
 use crate::knapsack::Knapsack;
 use crate::knapsack::Solution;
+
 /// Runs the Default heuristic for Knapsack Problem using the given data
 ///
 /// The Default heuristic for Knapsack Problem is a Greedy heuristic
@@ -8,13 +9,10 @@ use crate::knapsack::Solution;
 ///
 /// Necessary parameters:
 ///
-/// 1. The number of items in the instance (n)
-/// 2. A vector of i32 numbers representing the profits of each item
-/// 3. A vector of i32 numbers representing the weights of each item
-/// 4. The total capacity of the Knapsack
-/// 5. A boolean flag to log the execution of the algorithm
+/// 1. An instance of the Knapsack
+/// 2. A boolean flag to log the execution of the algorithm
 ///
-/// Returns a tuple with the total profit and the total weights of the solution.
+/// Returns a result to a Solution struct
 ///
 pub fn default(knapsack: &Knapsack, verbose: bool) -> Result<Solution, ()> {
     let mut in_kp = 0;
@@ -29,6 +27,58 @@ pub fn default(knapsack: &Knapsack, verbose: bool) -> Result<Solution, ()> {
             in_kp += knapsack.weights[i as usize];
             total_p += knapsack.profits[i as usize];
             solution[i as usize] = 1;
+        }
+
+        if verbose {
+            println!(
+                "I: {}, In_kp: {}, Total_p : {}, Solution: {:?}",
+                i, in_kp, total_p, solution
+            );
+        }
+        i += 1;
+    }
+
+    Ok(Solution::new(total_p, in_kp, &solution))
+}
+
+/// Runs the MaP heuristic for Knapsack Problem using the given data
+///
+/// The MaP heuristic for Knapsack Problem is a heuristic
+/// which takes the maximum profit available item in the set to include inside the Knapsack.
+///
+/// Necessary parameters:
+///
+/// 1. An instance of the Knapsack
+/// 2. A boolean flag to log the execution of the algorithm
+///
+/// Returns a result to a Solution struct
+///
+pub fn map(knapsack: &Knapsack, verbose: bool) -> Result<Solution, ()> {
+    let mut in_kp = 0;
+    let mut total_p = 0;
+    let mut i: u32 = 0;
+    let mut solution: Vec<u32> = vec![0; knapsack.n as usize];
+
+    let mut items = knapsack
+        .profits
+        .iter()
+        .zip(knapsack.weights.clone())
+        .enumerate()
+        .map(|(i, (p, w))| (*p, w, i))
+        .collect::<Vec<(u32, u32, usize)>>();
+
+    items.sort_by(|a, b| a.0.cmp(&b.0));
+    items.reverse();
+
+    loop {
+        if i >= knapsack.n {
+            break;
+        }
+        if items[i as usize].1 + in_kp <= knapsack.capacity {
+            in_kp += items[i as usize].1;
+            total_p += items[i as usize].0;
+            // We get the index of the item in the original knapsack
+            solution[items[i as usize].2] = 1;
         }
 
         if verbose {
