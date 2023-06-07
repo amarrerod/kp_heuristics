b0b8d8907ddb2a9ffe51a601f211e1da19829aaa
amarrerod <alemarrerodiaz@gmail.com>
2023-06-07 11:18:02 +0100

    🚨 Fixes lint warning

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use knapsack::Knapsack;
 fn main() {
     let n: u32 = 10;
     let mut knapsack = Knapsack::new(n, Some(100), None);
-    let ok = knapsack.create_random_items(1, 100);
+    let _ = knapsack.create_random_items(1, 100);
 
     println!("Knapsack: {:?}", knapsack);
 
